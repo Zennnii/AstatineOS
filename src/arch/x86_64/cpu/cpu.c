6836ed43469ee18c038c599864d4b0a9d383e7c8
VukProgramming <vukmitic81@gmail.com>
2026-08-31 19:39:23 +0200

    merge: resolve cpu.c conflict

--- src/arch/x86_64/cpu/cpu.c
+++ src/arch/x86_64/cpu/cpu.c
@@ -1,8 +1,6 @@
 #include <stdint.h>
 #include "arch/x86_64/cpu/cpu.h"
 
-<<<<<<< HEAD
-=======
 void hlt(void) {
     asm volatile ("hlt");
 }
@@ -15,7 +13,6 @@ void sti(void) {
     asm volatile ("sti");
 }
 
->>>>>>> 310d9ed13e1546b4283363d16acff34062825912
 // halt the cpu indefinitely
 void hcf(void) {
     cli();
@@ -24,21 +21,6 @@ void hcf(void) {
     }
 }
 
-<<<<<<< HEAD
-void hlt(void) {
-    asm volatile ("hlt");
-}
-
-void cli(void) {
-    asm volatile ("cli");
-}
-
-void sti(void) {
-    asm volatile ("sti");
-}
-
-=======
->>>>>>> 310d9ed13e1546b4283363d16acff34062825912
 uint64_t read_cr0(void) {
     uint64_t value;
 
@@ -72,11 +54,7 @@ uint64_t read_cr3(void) {
     return value;
 }
 
-<<<<<<< HEAD
 uint64_t read_cr4(void) {
-=======
-uint64_t read_cr2(void) {
->>>>>>> 310d9ed13e1546b4283363d16acff34062825912
     uint64_t value;
 
     asm volatile (
